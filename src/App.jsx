3013e0514304a0f0dffd8b9eafbc136c051aa0e9
snaskn <0zu3851p1-9196e@ezweb.ne.jp>
2023-08-26 20:16:13 +0900

    第三回まで

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // DO NOT DELETE
-
+import * as React from 'react'
 import './App.css'
+import { useState } from 'react'
 
 /**
  * @type {() => JSX.Element}
  */
+const [dogUrl, setDogUrl] = useState('https://dog.ceo/dog-api/')
+
 export const App = () => {
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>Dogapp</header>
+      <img src={dogUrl} alt="これがチワワです" />
     </div>
   )
-}
+};
+
