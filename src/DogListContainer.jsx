a1907930736a90b93452c0472184261391a55158
snaskn <0zu3851p1-9196e@ezweb.ne.jp>
2024-02-20 01:42:57 +0900

    問10をクリアした状態（cssはやっていない）

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,9 +1,13 @@
 // @ts-check
 import { BreedsSelect } from './BreedsSelect.jsx'
+import { DogImage } from './DogImage.jsx'
 import { useEffect, useState } from 'react'
+
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState([''])
+  const [selectedBreed, setSelectedBreed] = useState('')  
+  const [breedsPics, setBreedsPics] = useState([''])
 
   useEffect(() => {
     fetch('https://dog.ceo/api/breeds/list/all')
@@ -12,15 +16,36 @@ export const DogListContainer = () => {
         const breedsNames = Object.keys(data.message)
         setBreeds(breedsNames)
       })
-  })
+  }, [])
+
+  const handleSelectedChange = e => {
+    setSelectedBreed(e.target.value)
+  }
+  // useEffect(() => {
+  //   console.log(selectedBreed);
+  // }, [selectedBreed]);
+
+  const fetchAPI = () => {
+    if (!selectedBreed) return;
+    fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/12`)
+      .then(response => response.json())
+      .then(data => {
+        setBreedsPics(data.message)
+      })
+  }
 
   return (
     <div>
-      <ul>
-        {breeds.map((breed, index) => (
-          <li key={index}>{breed}</li>
+      <BreedsSelect 
+        breeds={breeds} 
+        selectedBreed={selectedBreed} 
+        handleSelectedChange={handleSelectedChange}
+      />
+      <button onClick={() => setBreedsPics([])}>リセット</button>
+      <button onClick={fetchAPI}>表示</button>
+      {breedsPics.map((url, index) => (
+          <DogImage key={index} imageUrl={url}/>
         ))}
-      </ul>
     </div>
   )
 }
