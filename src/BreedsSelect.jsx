cb7c08f877280823776aaf4587de8a24fc58022c
snaskn <0zu3851p1-9196e@ezweb.ne.jp>
2024-02-20 01:54:45 +0900

    問10をクリアした状態

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,17 @@
 // @ts-check
+import { useState } from 'react'
 
-export const BreedsSelect = () => {
-  return <></>
+export const BreedsSelect = ({ breeds, selectedBreed, handleSelectedChange }) => {
+
+  return (
+    <div>
+      <select value={selectedBreed} onChange={handleSelectedChange}>
+        {breeds.map(breed => (
+          <option key={breed}>{breed}</option>
+        ))}
+      </select>
+    </div>
+  )
 }
 
 export default BreedsSelect
